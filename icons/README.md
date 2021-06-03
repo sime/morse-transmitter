1abe6f6736b0ddb0f463ae5af2edd9110e59b8a5
Evan <evan-brass@protonmail.com>
2021-06-03 11:42:09 -0700

    Update icons / colors / sw version bump

---
+++ icons/README.md
@@ -0,0 +1,4 @@
+# Icons
+The icons are drawn to pngs for the PWA install stuff
+
+The favicon is just an optimized version of the logo svg.
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // This service worker will never refetch assets once it's installed the first time.  This means that in order for the app to update, the service worker must change and recache the assets.  Changing this verions number will do that.
-const version = "0.5"
+const version = "0.6"
 const static_cache_name = 'static_assets-' + version;
 
 
@@ -12,7 +12,7 @@ self.addEventListener('install', e => e.waitUntil((async () => {
 		'index.html',
 
 		'app.webmanifest',
-		'icons/logo-small.svg', // Needed for the page icon.
+		'icons/logo-opt.svg', // Needed for the page icon.
 
 		'style/index.css',
 		'style/Poppins/Poppins-Regular.ttf',
